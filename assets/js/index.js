d28ae7284a53485fa3cfcfeb5efc33387b42d6a7
Chris Sun <christopher.sun.sun@gmail.com>
2019-11-13 15:25:35 -0500

    Initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+(function ($) {
+
+    // scroll functions
+    $(window).scroll(function (e) {
+
+        // add/remove class to navbar when scrolling to hide/show
+        var scroll = $(window).scrollTop();
+        if (scroll >= 150) {
+            $('.navbar').addClass("navbar-hide");
+        } else {
+            $('.navbar').removeClass("navbar-hide");
+        }
+
+    });
+
+})(jQuery);    
